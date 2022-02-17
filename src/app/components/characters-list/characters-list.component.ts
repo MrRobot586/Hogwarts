6c1fd04ae19b902521261f4d996a0d9f82377e4a
Diego Oropeza <diegojoseoropezagmail.com>
2022-02-17 01:05:49 -0400

    Go to top button added

--- src/app/components/characters-list/characters-list.component.ts
+++ src/app/components/characters-list/characters-list.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+
 import { Character } from 'src/app/models/character.interface';
 import { HpApiServiceService } from 'src/app/services/hp-api-service.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+
 
 @Component({
   selector: 'app-characters-list',
@@ -10,10 +14,17 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class CharactersListComponent implements OnInit {
 
+  faArrowUp = faArrowUp;
   public query!:string;
   public characters!:Character[];
+
+  public GoUpbtn:boolean = false;
   
-  constructor(private api:HpApiServiceService,  private route: ActivatedRoute) { }
+  constructor(
+    private api:HpApiServiceService,
+    private route: ActivatedRoute,
+    @Inject(DOCUMENT) private document:Document
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
@@ -23,6 +34,25 @@ export class CharactersListComponent implements OnInit {
         this.characters = data;
       });
     });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll():void{
+    const yOffSet = window.pageYOffset;
+    if((yOffSet || this.document.documentElement.scrollTo || this.document.body.scrollTo) > 2000){
+      this.GoUpbtn = true;
+    }else if(this.GoUpbtn && (yOffSet || this.document.documentElement.scrollTo || this.document.body.scrollTo) < 200){
+      this.GoUpbtn = false;
+    }
+  }
+  
+  scrollTop():void{
+    this.document.body.scrollTop = 0;
+    this.document.documentElement.scrollTop = 0;
+  }
+
+  onScrollDown():void{
+    
   }
 
 }
